60326b183755d2feeb8d43b591678d81f8e014a9
jashhhhhhhhhhh <156871641+jashhhhhhhhhhh@users.noreply.github.com>
2024-02-22 17:14:29 -0500

    Update email.js

--- email.js
+++ email.js
@@ -1,12 +1,13 @@
 var visible = false;
-
-function showOrHideEmail() {
-    if (visible) {
-        document.getElementById('email').innerHTML = " Show my Email";
-        visible = false;
-    } else {
-        var myEmail = "<a href='mailto:bjaswanth24@gmail.com'>bjaswanth24@gmail.com</a>";
-        document.getElementById('email').innerHTML = myEmail;
-        visible = true;
-    }
+function showOrHideEmail(){
+  if (visible){
+    document.getElementById('email').innerHTML=" Show my Email";
+    visible=false;
+  }
+  else{
+    var myEmail="<a href='mailto:nakkantm" +"@"+
+                "mail.uc.edu'>nakkantm"+"@"+"mail.uc.edu</a>";
+    document.getElementById('email').innerHTML=myEmail;
+    visible= true;
+  }
 }
